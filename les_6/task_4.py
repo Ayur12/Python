b7d908c8879ca1dca8e9eee1ba36a6d99a60de83
Ayur12 <Ayur12@mail.ru>
2020-11-17 01:14:08 +0800

    task_4

--- les_6/task_4.py
+++ les_6/task_4.py
@@ -6,38 +6,70 @@
 При значении скорости свыше 60 (TownCar) и 40 (WorkCar) должно выводиться сообщение о превышении скорости.
 """
 
+import random
+
 
 class Car:
-    speed = 'speed'
-    color = 'color'
-    name = 'name'
-    is_police = True
+    speed = random.randint(1, 100)
+    color = random.choice(['black', 'white', 'red', 'blue', 'yellow'])
+    name = random.choice(['TownCar', 'SportCar', 'WorkCar', 'PoliceCar'])
+    is_police = random.choice([True, False])
+
     def go(self):
         print('Машина поехала')
+
     def stop(self):
         print('Машина остановилась')
+
     def turn(self):
-        print('машина повернула налево')
-        print('машина повернула направо')
+        direction = random.choice(['Налево', 'Направо'])
+        print(direction)
+
     def show_speed(self):
-        print('Текущая скорость')
+        print(f'Текущая скорость {self.speed}')
 
 
 class TownCar(Car):
-    def show_speed(self):
-        print('Текущая скорость')
+    def speed_limit(self):
+        max_speed = 60
+        if self.speed > max_speed:
+            print(f'Ваша скорость {self.speed}. Сбросьте скорость до 60 км/ч.')
+        elif 0 < self.speed < 60:
+            print(f'Ваша скорость {self.speed}')
 
 
 class SportCar(Car):
-    def show_speed(self):
-        print('Текущая скорость')
+    def model_car(self):
+        return self.name
 
 
 class WorkCar(Car):
-    def show_speed(self):
-        print('Текущая скорость')
+    def speed_limit(self):
+        max_speed = 40
+        if self.speed > max_speed:
+            print(f'Ваша скорость {self.speed}. Сбросьте скорость до 40 км/ч.')
+        elif 0 < self.speed < 60:
+            print(f'Ваша скорость {self.speed}')
 
 
 class PoliceCar(Car):
-    def show_speed(self):
-        print('Текущая скорость')
+    def police(self):
+        if self.is_police == True:
+            print('Это полицейская машина')
+        else:
+            print('Это не полицейская машина')
+
+car = Car()
+car.turn()
+car.turn()
+
+bus = TownCar()
+bus.speed_limit()
+print(f'Цвет автобуса {bus.color}')
+
+sport = SportCar()
+print(f'Вы выбрали {sport.name}')
+sport.go()
+sport.show_speed()
+cop = PoliceCar()
+cop.police()
